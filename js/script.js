ec3521eabeb2fc5e9b98554146b5cd75906e1d01
Vialaik <Vialaik@gmail.com>
2023-09-25 17:34:37 +0200

    типы данных

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log(2);
+"use strict";
+
+// переменные
+let number5 = 5;
+let NUmber = 5;
+
+const primeryConst = 34;
+
+// типы данних
+
+// number = 24
+// string = 'hfbvf'
+// booleans = false / true
+// null
+// undefined
+// Symbol
+// BigInt
