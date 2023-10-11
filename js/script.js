29af5f940bdd09a518041267fb48d3e36b4acc19
Vialaik <Vialaik@gmail.com>
2023-10-11 14:10:08 +0200

    lesson 23

--- js/script.js
+++ js/script.js
@@ -33,13 +33,110 @@ const personalMovieDB = {
    privat: true,
 };
 
-let lastVistFilm = prompt("Один из последних просмотренных фильмов?", ""),
-   lastVilmReiting = prompt("На сколько оцените его?", ""),
-   lastVistFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
-   lastVilmReiting2 = prompt("На сколько оцените его?", "");
+// let lastVistFilm = prompt("Один из последних просмотренных фильмов?", ""),
+//    lastVilmReiting = prompt("На сколько оцените его?", ""),
+//    lastVistFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
+//    lastVilmReiting2 = prompt("На сколько оцените его?", "");
 
 personalMovieDB.movies[lastVistFilm] = lastVilmReiting;
 personalMovieDB.movies[lastVistFilm2] = lastVilmReiting2;
 
 console.log(personalMovieDB);
 // console.log(typeof personalMovieDB.count);
+
+// _______________________________________________________________
+//                         Задачи по циклам
+//----------------------------------------------------------------
+
+for (let index = 5; index <= 10; index++) {
+   console.log(index);
+}
+
+for (let index = 20; index > 10; index--) {
+   if (index == 13) {
+      break;
+   }
+   console.log(index);
+}
+
+for (let index = 2; index <= 10; index++) {
+   switch (index) {
+      case 2:
+         console.log(index);
+         break;
+
+      case 3:
+         continue;
+      case 4:
+         console.log(index);
+         break;
+
+      case 5:
+         continue;
+      case 6:
+         console.log(index);
+         break;
+      case 7:
+         continue;
+      case 8:
+         console.log(index);
+         break;
+      case 9:
+         continue;
+      case 10:
+         console.log(index);
+         break;
+      default:
+         break;
+   }
+}
+
+for (let i = 2; i <= 16; i++) {
+   if (i % 2 === 0) {
+      continue;
+   } else {
+      console.log(i);
+   }
+}
+
+let i = 2;
+while (i < 16) {
+   i++;
+   if (i % 2 === 0) {
+      continue;
+   } else {
+      console.log(i);
+   }
+}
+
+const arrayOfNumbers = [];
+for (let index = 5; index <= 10; index++) {
+   arrayOfNumbers[index - 5] = index;
+}
+console.log(arrayOfNumbers);
+
+// 2 часть
+// ------------------------------------------------
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for (let index = 0; index < arr.length; index++) {
+//    result[index] = arr[index];
+// }
+// console.log(result);
+
+// const data = [5, 10, "Shopping", 20, "Homework"];
+for (let index = 0; index < data.length; index++) {
+   if (typeof data[index] === "number") {
+      data[index] = data[index] * 2;
+   } else if (typeof data[index] === "string") {
+      data[index] = `${data[index]} - done`;
+   }
+}
+// console.log(data);
+
+const data = [5, 10, "Shopping", 20, "Homework"];
+
+for (let i = 1; i <= data.length; i++) {
+   result[i - 1] = data[data.length - i];
+}
