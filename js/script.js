5758bf019c19ff4bfb29ac551f698c5a475a655f
Vialaik <Vialaik@gmail.com>
2023-10-11 18:33:55 +0200

    lesson 24

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@
 
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "36");
 
 const personalMovieDB = {
    count: numberOfFilms,
@@ -38,105 +38,30 @@ const personalMovieDB = {
 //    lastVistFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
 //    lastVilmReiting2 = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[lastVistFilm] = lastVilmReiting;
-personalMovieDB.movies[lastVistFilm2] = lastVilmReiting2;
+for (let i = 0; i < 2; i++) {
+   let lastVistFilm = prompt(`Один из последних просмотренных фильмов? ${i}`, `nfldbld k,s'd'sdv${i}`);
+   let lastVilmReiting = prompt(`На сколько оцените его?${i}`, `${1}`);
+   if (
+      lastVistFilm != "" &&
+      lastVistFilm != null &&
+      lastVistFilm.length <= 50 &&
+      lastVilmReiting != "" &&
+      lastVilmReiting != null &&
+      lastVilmReiting.length <= 50
+   ) {
+      personalMovieDB.movies[lastVistFilm] = lastVilmReiting;
+   } else {
+      i--;
+   }
+}
+if (personalMovieDB.count < 10) {
+   alert("10- просмотров");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 10) {
+   alert("10+ просмотров");
+} else if (personalMovieDB.count > 30) {
+   alert("30+ просмотров");
+}
+// personalMovieDB.movies[lastVistFilm] = lastVilmReiting;
+// personalMovieDB.movies[lastVistFilm2] = lastVilmReiting2;
 
 console.log(personalMovieDB);
-// console.log(typeof personalMovieDB.count);
-
-// _______________________________________________________________
-//                         Задачи по циклам
-//----------------------------------------------------------------
-
-for (let index = 5; index <= 10; index++) {
-   console.log(index);
-}
-
-for (let index = 20; index > 10; index--) {
-   if (index == 13) {
-      break;
-   }
-   console.log(index);
-}
-
-for (let index = 2; index <= 10; index++) {
-   switch (index) {
-      case 2:
-         console.log(index);
-         break;
-
-      case 3:
-         continue;
-      case 4:
-         console.log(index);
-         break;
-
-      case 5:
-         continue;
-      case 6:
-         console.log(index);
-         break;
-      case 7:
-         continue;
-      case 8:
-         console.log(index);
-         break;
-      case 9:
-         continue;
-      case 10:
-         console.log(index);
-         break;
-      default:
-         break;
-   }
-}
-
-for (let i = 2; i <= 16; i++) {
-   if (i % 2 === 0) {
-      continue;
-   } else {
-      console.log(i);
-   }
-}
-
-let i = 2;
-while (i < 16) {
-   i++;
-   if (i % 2 === 0) {
-      continue;
-   } else {
-      console.log(i);
-   }
-}
-
-const arrayOfNumbers = [];
-for (let index = 5; index <= 10; index++) {
-   arrayOfNumbers[index - 5] = index;
-}
-console.log(arrayOfNumbers);
-
-// 2 часть
-// ------------------------------------------------
-
-// const arr = [3, 5, 8, 16, 20, 23, 50];
-// const result = [];
-// for (let index = 0; index < arr.length; index++) {
-//    result[index] = arr[index];
-// }
-// console.log(result);
-
-// const data = [5, 10, "Shopping", 20, "Homework"];
-for (let index = 0; index < data.length; index++) {
-   if (typeof data[index] === "number") {
-      data[index] = data[index] * 2;
-   } else if (typeof data[index] === "string") {
-      data[index] = `${data[index]} - done`;
-   }
-}
-// console.log(data);
-
-const data = [5, 10, "Shopping", 20, "Homework"];
-
-for (let i = 1; i <= data.length; i++) {
-   result[i - 1] = data[data.length - i];
-}
